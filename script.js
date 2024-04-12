6e7ab93a642e26bfc1ecdcb75824bc8e1c194e86
faf16 <fasikawbirhan7@gmail.com>
2024-04-12 11:53:13 -0700

    initial-commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const emailButton = document.getElementById("emailButton");
+const githubButton = document.getElementById("githubButton");
+
+emailButton.addEventListener("click", function () {
+  window.location.href = "mailto: fasikawbirhan7@gmail.com";
+});
+
+githubButton.addEventListener("click", function () {
+  window.location.href = "https://github.com/faf16";
+});
